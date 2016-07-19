85ae4a979aaa7d4b8bbfb2dff92c23050ba5e397
Marios Katsos <keatmar@gmail.com>
2016-07-19 19:27:47 +0300

    version 1.3

--- WebApps/src/servlets/profile/Login.java
+++ WebApps/src/servlets/profile/Login.java
@@ -30,57 +30,47 @@ public class Login extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		RequestDispatcher disp;
 		String username = request.getParameter("txtUserName");
 		String password = request.getParameter("txtPassword");
 
-		/* Administrator login */
-		if (username.equals("admin") && password.equals("admin")) {
+		UsersDAO userDAO = new UsersDAOImpl();
+		User user = userDAO.loginUser(username, password);
+		/* user Login */
 
+		if (user != null) {
+			/* Hold session */
 			HttpSession session = request.getSession();
-			session.setAttribute("username", "Admin");
-			disp = getServletContext().getRequestDispatcher("/admin.jsp");
-			disp.forward(request, response);
+			session.setAttribute("id", user.getId());
+			session.setAttribute("username", user.getUsername());
+			session.setAttribute("password", user.getPassword());
+			session.setAttribute("first_name", user.getFirstName());
+			session.setAttribute("last_name", user.getLastName());
+			session.setAttribute("email", user.getEmail());
+			session.setAttribute("phone", user.getPhone());
+			session.setAttribute("address", user.getAddress());
+			session.setAttribute("role", user.getRole());
+			session.setAttribute("city", user.getCity());
+			session.setAttribute("afm", user.getAfm());
+			session.setAttribute("zip", user.getZip());
+			session.setAttribute("password", user.getPassword());
 
-		} else {
-			UsersDAO userDAO = new UsersDAOImpl();
-			User user = userDAO.loginUser(username, password);
-			/* user Login */
-			
-			if (user != null) {
-				/* Hold session */
-				HttpSession session = request.getSession();
-				session.setAttribute("id",user.getId());
-				session.setAttribute("username", user.getUsername());
-				session.setAttribute("password",user.getPassword());
-				session.setAttribute("first_name",user.getFirstName());
-				session.setAttribute("last_name",user.getLastName());
-				session.setAttribute("email",user.getEmail());
-				session.setAttribute("phone",user.getPhone());
-				session.setAttribute("address",user.getAddress());
-				session.setAttribute("role",user.getRole());
-				session.setAttribute("city",user.getCity());
-				session.setAttribute("afm",user.getAfm());
-				session.setAttribute("zip",user.getZip());
-				session.setAttribute("password",user.getPassword() );
-				
-				if (user.getActive() == false) {
-					disp = getServletContext().getRequestDispatcher("/need_approve.jsp");
-					disp.forward(request, response);
-				//	response.sendRedirect("http://localhost:8080/WebApps/need_approve.jsp");
-				} else {
-					disp = getServletContext().getRequestDispatcher("/index.jsp");
-					disp.forward(request, response);
-					//response.sendRedirect("http://localhost:8080/WebApps");
-				}
-				/* user do not login */
-			} else {
-				disp = getServletContext().getRequestDispatcher("/login.jsp");
+			if (user.getActive() == false) {
+				disp = getServletContext().getRequestDispatcher("/need_approve.jsp");
 				disp.forward(request, response);
+				// response.sendRedirect("http://localhost:8080/WebApps/need_approve.jsp");
+			} else {
+				disp = getServletContext().getRequestDispatcher("/index.jsp");
+				disp.forward(request, response);
+				// response.sendRedirect("http://localhost:8080/WebApps");
 			}
-			
-		}		
+			/* user do not login */
+		} else {
+			disp = getServletContext().getRequestDispatcher("/login.jsp");
+			disp.forward(request, response);
+		}
+
 	}
 
 }
